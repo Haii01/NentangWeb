fd3e72a53045e383fc1581412f93c565f128f89c
Haii01 <tangchien77@gmail.com>
2026-05-18 04:58:09 +0700

    BTL

---
+++ session_02_bootstrap/projects/portfolio_bootstrap/js/main.js
@@ -0,0 +1,22 @@
+const skillBars = document.querySelectorAll(".skill-progress");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            const bar = entry.target;
+
+            const width = bar.getAttribute("data-width");
+
+            bar.style.width = width;
+        }
+
+    });
+
+});
+
+skillBars.forEach((bar) => {
+    observer.observe(bar);
+});
