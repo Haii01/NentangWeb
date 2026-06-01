e686e13b1e9f4e9b051595604924fdcd5507a56a
Haii01 <tangchien77@gmail.com>
2026-06-01 23:10:45 +0700

    [COMPONENT] Create Hero component

---
+++ session_04_react_basics/session_04_portfolio_react/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="container">
+        <h1>
+          Hi, I'm <span className="highlight">Hai</span>
+        </h1>
+
+        <p>
+          Frontend Developer | React Developer
+        </p>
+
+        <a href="#portfolio" className="btn">
+          View My Work
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
