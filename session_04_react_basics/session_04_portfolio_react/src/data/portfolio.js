a2e14872ef1fe613650b93207ff7bd2bb0c1be51
Haii01 <tangchien77@gmail.com>
2026-06-01 23:09:49 +0700

    [COMPONENT] Create Header component

---
+++ session_04_react_basics/session_04_portfolio_react/src/data/portfolio.js
@@ -0,0 +1,7 @@
+export const skills = [
+    { id: 1, name: "HTML5", level: 95 },
+    { id: 2, name: "CSS3", level: 90 },
+    { id: 3, name: "JavaScript", level: 85 },
+    { id: 4, name: "React", level: 80 },
+    { id: 5, name: "Node.js", level: 70 }
+];
