b41fb440d07e671e648adebb7cb78fd0578bcdbf
Haii01 <tangchien77@gmail.com>
2026-06-01 23:24:26 +0700

    [LAYOUT] Assemble App.jsx layout

---
+++ session_04_react_basics/session_04_portfolio_react/src/components/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <section id="about" className="section">
+      <div className="container">
+        <h2>About Me</h2>
+
+        <p>
+          Tôi là sinh viên CNTT yêu thích lập trình web và React.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ session_04_react_basics/session_04_portfolio_react/src/components/Skills.jsx
@@ -0,0 +1,19 @@
+import { skills } from "../data/portfolio";
+
+function Skills() {
+  return (
+    <section id="skills" className="section">
+      <div className="container">
+        <h2>Skills</h2>
+
+        {skills.map((skill) => (
+          <div key={skill.id}>
+            {skill.name} - {skill.level}%
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
